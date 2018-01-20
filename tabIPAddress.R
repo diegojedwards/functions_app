bdf0bf48599488cab46e5af862ccdc749848582a
diegojedwards <diegojedwards@users.noreply.github.com>
2018-01-20 16:00:06 -0300

    Delete tabIPAddress.R

--- tabIPAddress.R
+++
@@ -1,9 +0,0 @@
-tabIPAddress <- function(datos) {
-  
-  datos %>%
-    group_by(ipaddr) %>%
-    summarise(count = n()) %>%
-    filter(count >= 2) %>%
-    ungroup() %>%
-    arrange(desc(count))
-}
